0c9b707e8e363d0a0e8818fe09a0bfdb1df167e4
SJHuang1123 <137044423+SJHuang1123@users.noreply.github.com>
2024-11-25 08:12:44 +0800

    Update common_utils.py

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -195,7 +195,7 @@ def torch_to_np(img_var):
     return img_var.detach().cpu().numpy()[0]
 
 
-def optimize(optimizer_type, parameters, closure, LR, num_iter):
+def optimize(optimizer_type, parameters, closure, LR, num_iter, betas):
     """Runs optimization loop.
 
     Args:
@@ -205,24 +205,9 @@ def optimize(optimizer_type, parameters, closure, LR, num_iter):
         LR: learning rate
         num_iter: number of iterations 
     """
-    if optimizer_type == 'LBFGS':
-        # Do several steps with adam first
-        optimizer = torch.optim.Adam(parameters, lr=0.001)
-        for j in range(100):
-            optimizer.zero_grad()
-            closure()
-            optimizer.step()
-
-        print('Starting optimization with LBFGS')        
-        def closure2():
-            optimizer.zero_grad()
-            return closure()
-        optimizer = torch.optim.LBFGS(parameters, max_iter=num_iter, lr=LR, tolerance_grad=-1, tolerance_change=-1)
-        optimizer.step(closure2)
-
-    elif optimizer_type == 'adam':
+    if optimizer_type == 'adam':
         print('Starting optimization with ADAM')
-        optimizer = torch.optim.Adam(parameters, lr=LR)
+        optimizer = torch.optim.Adam(parameters, betas = betas, lr=LR)
         
         for j in range(num_iter):
             optimizer.zero_grad()
